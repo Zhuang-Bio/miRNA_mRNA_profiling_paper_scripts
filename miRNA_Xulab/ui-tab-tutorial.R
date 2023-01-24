e2c55a5b70958dfc1938dad21570ff9ef4d08478
Zhuang Liu <55880584+Zhuang-Bio@users.noreply.github.com>
2023-01-24 15:21:34 +0100

    Delete miRNA_Xulab directory

--- miRNA_Xulab/ui-tab-tutorial.R
+++
@@ -1,8 +0,0 @@
-tabItem(tabName = "startTutorial",
-  fluidRow(
-    column(12,
-           htmltools::tags$iframe(src = "Tutorial.html", width = '100%',  height = '2400',  style = "border:none;")
-    )
-  )
-)
-
--- miRNA_Xulab/vedio/readme.txt
+++
@@ -1,1 +0,0 @@
-# render the markdown file and put it in the www folder, otherwise error occurs.
